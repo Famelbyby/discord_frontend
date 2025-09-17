442999160b3d328a415dc3bc6dc531595c3b4914
lexa <lexagorbunov14@gmail.com>
2025-09-17 18:49:07 +0300

    DS-8: removed babel config

--- app/friends/layout.tsx
+++ app/friends/layout.tsx
@@ -2,14 +2,14 @@
 
 import { useState } from 'react';
 import FriendsHeader from '../../src/core/friends/friend.header';
-import { HeaderTitle } from '../utils/friends/types';
+import { FriendsHeaderTitle } from '../../src/utils/types/friends';
 
 export default function FriendsLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
-	const [selectedItem, chooseItem] = useState<HeaderTitle>('Поиск');
+	const [selectedItem, chooseItem] = useState<FriendsHeaderTitle>('Поиск');
 
 	return (
 		<div className="friends-layout">
