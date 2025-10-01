51da217e487dee51ad361dfdc48107fa334a85d6
lexa <lexagorbunov14@gmail.com>
2025-10-01 19:11:39 +0300

    DS-16: changed rule from error to warn

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -25,7 +25,7 @@ const eslintConfig = [
             '@typescript-eslint/no-unused-vars': 'error',
             '@typescript-eslint/no-explicit-any': 'warn',
             "react-hooks/exhaustive-deps": 'warn',
-            'no-console': 'error',
+            'no-console': 'warn',
         },
     }),
 ];
