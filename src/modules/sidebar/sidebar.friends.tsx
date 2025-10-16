4aeec063cab378d04e66deb7bdae025c16d0e2a0
lexa <lexagorbunov14@gmail.com>
2025-10-16 14:38:34 +0300

    DS-5: add incomings count from API

--- src/modules/sidebar/sidebar.friends.tsx
+++ src/modules/sidebar/sidebar.friends.tsx
@@ -1,10 +1,28 @@
+'use client';
+
+import { GetIncomings } from '@/src/api/users/incoming';
 import SidebarFriendsHeader from '@/src/core/sidebar/sidebar-friends/sidebar.header';
+import { useUserStore } from '@/src/stores/user.store';
 import '@/src/styles/sidebar/sidebar-friends/general.style.scss';
+import { useEffect, useState } from 'react';
 
 export default function SidebarFriends() {
+	const { id } = useUserStore();
+	const [incomingsCount, setIncomingsCount] = useState(0);
+
+	useEffect(() => {
+		async function updateIncomingsCount() {
+			const incomings = await GetIncomings(id);
+
+			setIncomingsCount(incomings.length);
+		}
+
+		updateIncomingsCount();
+	});
+
 	return (
 		<div className="sidebar-friends">
-			<SidebarFriendsHeader incomingsCount={1} />
+			<SidebarFriendsHeader incomingsCount={incomingsCount} />
 		</div>
 	);
 }
