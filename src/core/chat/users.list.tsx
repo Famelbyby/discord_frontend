03fd85e540a9c88688d8cbd3be6a570c6d993574
lexa <lexagorbunov14@gmail.com>
2025-11-11 19:07:23 +0300

    DS-34: remade to custom link

--- src/core/chat/users.list.tsx
+++ src/core/chat/users.list.tsx
@@ -7,30 +7,25 @@ import {
 import { USERS_URL } from '@/src/utils/constants/shared/URLs/front.urls';
 import { IChatUsersList } from '@/src/utils/types/chat/users.list';
 import Image from 'next/image';
-import { useRouter } from 'next/navigation';
 import '@/src/styles/chat/users.list.style.scss';
+import { CustomLink } from '../shared/CustomLink';
 
 export default function ChatUsersList({ lead_id, users }: IChatUsersList) {
-	const router = useRouter();
-
-	function onUserAvatarClick(userId: string) {
-		router.push(`${USERS_URL}/${userId}`);
-	}
-
 	return (
 		<div className="chat-users-list">
 			{users.map((user) => {
 				return (
 					<div className="chat-users-list-item" key={user.id}>
-						<Image
-							className={`chat-users-list-item__img ${lead_id === user.id ? 'chat-users-list-item__img_leader' : ''}`}
-							width={USER_AVATAR_WIDTH}
-							height={USER_AVATAR_HEIGHT}
-							src={user.avatarUrl}
-							alt={user.username}
-							title={user.username}
-							onClick={() => onUserAvatarClick(user.id)}
-						/>
+						<CustomLink href={`${USERS_URL}/${user.id}`}>
+							<Image
+								className={`chat-users-list-item__img ${lead_id === user.id ? 'chat-users-list-item__img_leader' : ''}`}
+								width={USER_AVATAR_WIDTH}
+								height={USER_AVATAR_HEIGHT}
+								src={user.avatarUrl}
+								alt={user.username}
+								title={user.username}
+							/>
+						</CustomLink>
 					</div>
 				);
 			})}
