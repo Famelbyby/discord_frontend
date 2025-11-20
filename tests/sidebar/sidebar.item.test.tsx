dc9065da7c70a994afe21fc0ed0d80eab6ec1c21
lexa <lexagorbunov14@gmail.com>
2025-11-20 20:31:32 +0300

    DS-24: fixed test errors

--- tests/sidebar/sidebar.item.test.tsx
+++ tests/sidebar/sidebar.item.test.tsx
@@ -1,28 +1,30 @@
-import SidebarItem from '@/src/core/sidebar/sidebar.item';
+import SidebarChatItem from '@/src/core/sidebar/sidebar.chat-item';
 import { ISidebarItem } from '@/src/utils/types/sidebar/sidebar.item';
 import { render, screen } from '@testing-library/react';
 
 const defaultProps: ISidebarItem = {
 	avatarUrl: '/shared/block.png',
 	id: '1',
-	name: 'ahah',
+	username: 'ahah',
 };
 
 describe('sidebar item', () => {
 	test('renders main sections', () => {
-		const { container } = render(<SidebarItem {...defaultProps} />);
+		const { container } = render(<SidebarChatItem {...defaultProps} />);
 
 		expect(
-			container.querySelector('.sidebar-item-wrapper')
+			container.querySelector('.sidebar-chat-item-wrapper')
 		).toBeInTheDocument();
-		expect(container.querySelector('.sidebar-item')).toBeInTheDocument();
 		expect(
-			container.querySelector('.sidebar-item__avatar')
+			container.querySelector('.sidebar-chat-item')
+		).toBeInTheDocument();
+		expect(
+			container.querySelector('.sidebar-chat-item__avatar')
 		).toBeInTheDocument();
 	});
 
 	test('should render with correct props', () => {
-		const { container } = render(<SidebarItem {...defaultProps} />);
+		const { container } = render(<SidebarChatItem {...defaultProps} />);
 
 		expect(
 			container.querySelector('[href="/profile/1"]')
